e4e8a55968cd16bc255b84f4ed2ba893530bfef9
brandon <bdickson1212@gmail.com>
2022-03-19 10:25:13 +0000

    fix: add apollo driver to graphql setup - Story 1

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -2,9 +2,19 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { MongooseModule } from '@nestjs/mongoose';
+import { GraphQLModule } from '@nestjs/graphql';
+import { PostsModule } from './post/post.module';
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
 
 @Module({
-  imports: [MongooseModule.forRoot('mongodb://localhost/nest')],
+  imports: [
+    MongooseModule.forRoot('mongodb://localhost/nest'),
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      autoSchemaFile: 'schema.gql',
+      driver: ApolloDriver,
+    }),
+    PostsModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
