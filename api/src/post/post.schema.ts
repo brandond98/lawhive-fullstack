892b85a1720a0c403704e20ed7cba27d4c024a28
brandon <bdickson1212@gmail.com>
2022-03-19 10:23:03 +0000

    refactor: add ID to Post schema

--- api/src/post/post.schema.ts
+++ api/src/post/post.schema.ts
@@ -1,18 +1,36 @@
 import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
 import { Document } from 'mongoose';
+import { ObjectType, Field, InputType, ID } from '@nestjs/graphql';
 
 export type PostDocument = Post & Document;
 
+@ObjectType()
 @Schema()
 export class Post {
+  @Field(() => ID)
+  @Prop()
+  _id: number;
+
+  @Field()
   @Prop({ required: true, maxlength: 25 })
   title: string;
 
+  @Field()
   @Prop({ required: true })
   description: string;
 
+  @Field()
   @Prop({ default: 'started' })
   state: string;
+}
+
+@InputType()
+export class CreatePostInput {
+  @Field()
+  title: string;
+
+  @Field()
+  description: string;
 }
 
 export const PostSchema = SchemaFactory.createForClass(Post);
