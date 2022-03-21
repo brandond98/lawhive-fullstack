fe32502d21d3fc2a82d4e9fe23243dddaf952d89
brandon <bdickson1212@gmail.com>
2022-03-21 22:20:58 +0000

    refactor: add simple error handling for scraper

--- api/src/scraper/scraper.service.ts
+++ api/src/scraper/scraper.service.ts
@@ -4,24 +4,32 @@ import * as puppeteer from 'puppeteer';
 @Injectable()
 export class ScraperService {
   async getData(url: string) {
-    // Launch browser
-    const browser = await puppeteer.launch();
-    const page = await browser.newPage();
-    // Navigate to url specified
-    await page.goto(url, {
-      waitUntil: 'networkidle2',
-    });
-    // Loop through text and add to array
-    const results = await page.evaluate(() => {
-      const contentList = [];
-      document
-        .querySelectorAll('[data-component="text-block"]')
-        .forEach((el) => {
-          contentList.push(el.textContent);
-        });
-      // Return text joined together
-      return contentList.join(' ');
-    });
-    return results;
+    try {
+      // Launch browser
+      const browser = await puppeteer.launch();
+      const page = await browser.newPage();
+      // Navigate to url specified
+      await page.goto(url, {
+        waitUntil: 'networkidle2',
+      });
+      // Loop through text and add to array
+      const results = await page.evaluate(() => {
+        const contentList = [];
+        document
+          .querySelectorAll('[data-component="text-block"]')
+          .forEach((el) => {
+            contentList.push(el.textContent);
+          });
+        // Return text joined together
+        return contentList.join(' ');
+      });
+      if (results.length) {
+        return results;
+      } else {
+        return 'Unable to pull content';
+      }
+    } catch (error) {
+      throw Error(error);
+    }
   }
 }
